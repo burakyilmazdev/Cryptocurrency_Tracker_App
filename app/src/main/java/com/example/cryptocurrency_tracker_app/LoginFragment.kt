cc1977f2605fa771f11ba5d184f16f895b42e3e7
burakyilmazdev <burakyilmaz1627@gmail.com>
2022-03-17 05:57:49 +0300

    View binding and navigation component have been integrated.

--- app/src/main/java/com/example/cryptocurrency_tracker_app/LoginFragment.kt
+++ app/src/main/java/com/example/cryptocurrency_tracker_app/LoginFragment.kt
@@ -1,25 +1,33 @@
 package com.example.cryptocurrency_tracker_app
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
+import com.example.cryptocurrency_tracker_app.databinding.FragmentLoginBinding
 
 
 class LoginFragment : Fragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private lateinit var binding: FragmentLoginBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+
+        binding.createAccount.setOnClickListener {
+            NavHostFragment.findNavController(this).navigate(R.id.action_loginFragment_to_signUpFragment)
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return binding.root
     }
 
 
--- app/src/main/java/com/example/cryptocurrency_tracker_app/MainActivity.kt
+++ app/src/main/java/com/example/cryptocurrency_tracker_app/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.cryptocurrency_tracker_app
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.View
+import android.widget.Button
+import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
